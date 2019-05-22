80c7c974a10bfedcf7daae7e62c3665f37aad7c5
Renzo Roso <renzo@geex.company>
2019-05-22 22:20:46 +0200

    • Updated template after notores/user has been added     • Frontend templates     • Refactored some server.js statements after notores/core updates • Added @shelf/jest-mongodb for mongo testing

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const sessions = require('client-sessions');
-const crypto = require('crypto');
-const passport = require('passport');
+const logger = require('@notores/core/logger')(module);
 const {middlewareForRouter} = require('@notores/core');
 
 module.exports = () => {
-    console.log('applying middlewares')
+    logger.info('applying middlewares');
     middlewareForRouter(
         express.static('./public/bower'),
         '/components',
@@ -18,28 +16,5 @@ module.exports = () => {
         express.static('./public/uploads'),
         '/uploads',
     );
-
-    middlewareForRouter(sessions({
-        cookieName: 'notores', // cookie name dictates the key name added to the request object
-        requestKey: 'session', // changes the session key from req.cookieName to this (req.session)
-        secret: process.env.COOKIE_SECRET, // should be a large unguessable string
-        duration: 20 * 7 * 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
-        activeDuration: 20 * 7 * 24 * 60 * 60 * 1000, // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
-        cookie: {
-            httpOnly: true, // when true, cookie is not accessible from javascript
-        }
-    }));
-
-    middlewareForRouter((req, res, next) => {
-        if (!req.session.id) {
-            const buf = crypto.randomBytes(16);
-            req.notores.id = buf.toString('hex');
-            // console.log(req.wsSession);
-        }
-        next();
-    });
-
-    middlewareForRouter(passport.initialize());
-    middlewareForRouter(passport.session());
 };
 
